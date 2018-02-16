ab441da7f28e9ff4321ab2f4b79438284485a066
Stephen Adams <tsadams@gmail.com>
2018-02-16 11:18:43 -0500

    Send requests to a url
    - testing a thing some more

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print('Hello World')
+import requests
+import json
+import os
+
+from time import sleep
+
+
+URL = os.environ.get('URL')
+
+while True:
+    requests.post(URL, json=json.dumps('{"test": "value"}'))
+    sleep(20)
